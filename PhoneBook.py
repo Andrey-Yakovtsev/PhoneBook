a8eb54b7307819715dab94fe788e6f60ad2a334d
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2020-07-03 12:18:37 +0300

    Борьба с ФОнбуком

--- PhoneBook.py
+++ PhoneBook.py
@@ -1,41 +1,24 @@
 from pprint import pprint
-from dataset import contacts_list
-
-
-# class PhoneBook:
-#
-#     def show_contacts(self):
-#         return pprint(contacts_list)
-#
-#     def add_new_contact(self):
-#         return
-#
-#     def delete_by_phone_number(self):
-#         return
-#
-#     def show_all_favourites(self):
-#         return
-#
-#     def find_by_name_surname(self):
-#         return
+# from dataset import contacts_list
 
 
 class Contact:
+    # contacts_list = []
     def __init__(self, first_name, second_name, phone_number=str, is_favourite='нет', **Extra_info):
         self.first_name = first_name
         self.second_name = second_name
         self.phone_number = phone_number
         self.is_favourite = is_favourite
-        # self.email = email,
         self.Extra_info = Extra_info
-
-        contacts_list.append(self.__dict__)
+        # self.contacts_list.append(self.__dict__)
 
     def add_to_favourite(self):
         self.is_favourite = 'да'
 
     def delete_from_favourite(self):
         self.is_favourite = 'нет'
+
+
 
     def __str__(self):
         return f'Имя: {self.first_name}\n' \
@@ -47,7 +30,6 @@ class Contact:
                f'telegram: {self.Extra_info["telegram"]}\n\t' \
                f'facebook: {self.Extra_info["facebook"]}'
 
-
 Vasyan = Contact(
     'Vasyan',
     'Chetvertiy',
@@ -56,6 +38,32 @@ Vasyan = Contact(
     telegram='@nekiy',
     facebook='www.facebook.com/whatever'
 )
+Petka = Contact(
+    'Пятый',
+    'Петька',
+    '+7555555555',
+    email='555@fifth.com',
+    telegram='@555',
+    facebook='www.facebook.com/fivestar'
+)
 Vasyan.add_to_favourite()
+# print(Vasyan)
+# print(Petka)
 
-print(Vasyan)
+class PhoneBook:
+
+    def __init__(self, name):
+        self.name = name
+        self.contacts_list = []
+
+    def show_contacts(self):
+        for item in self.contacts_list:
+            pprint(item)
+
+    def add_new_contact(self, Contact):
+        self.contacts_list.append(Contact)
+        # self.contacts_list.append(self.__dict__)
+
+MyTopContacts = PhoneBook
+MyTopContacts.add_new_contact(Petka)
+# MyTopContacts.show_contacts()
