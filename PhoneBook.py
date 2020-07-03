29df9ce709abffeab0f5f6d08533c9a353e54feb
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2020-07-03 12:50:16 +0300

    отовая версия

--- PhoneBook.py
+++ PhoneBook.py
@@ -1,24 +1,18 @@
-from pprint import pprint
-# from dataset import contacts_list
-
 
 class Contact:
-    # contacts_list = []
+
     def __init__(self, first_name, second_name, phone_number=str, is_favourite='нет', **Extra_info):
         self.first_name = first_name
         self.second_name = second_name
         self.phone_number = phone_number
         self.is_favourite = is_favourite
         self.Extra_info = Extra_info
-        # self.contacts_list.append(self.__dict__)
 
     def add_to_favourite(self):
         self.is_favourite = 'да'
 
     def delete_from_favourite(self):
         self.is_favourite = 'нет'
-
-
 
     def __str__(self):
         return f'Имя: {self.first_name}\n' \
@@ -58,12 +52,30 @@ class PhoneBook:
 
     def show_contacts(self):
         for item in self.contacts_list:
-            pprint(item)
+            print(item)
 
     def add_new_contact(self, Contact):
         self.contacts_list.append(Contact)
-        # self.contacts_list.append(self.__dict__)
 
-MyTopContacts = PhoneBook
+    def show_favourites(self):
+        for item in self.contacts_list:
+            if item.is_favourite == 'да':
+                print(item.phone_number)
+
+    def find_contact(self, first_name, second_name):
+        for item in self.contacts_list:
+            if item.first_name == first_name and item.second_name == second_name:
+                print(item.phone_number)
+
+MyTopContacts = PhoneBook('Моя телефонная книга')
 MyTopContacts.add_new_contact(Petka)
-# MyTopContacts.show_contacts()
+MyTopContacts.add_new_contact(Vasyan)
+print()
+print('Here are all contacts:')
+MyTopContacts.show_contacts()
+print()
+print('Here are favs:')
+MyTopContacts.show_favourites()
+print()
+print('Here is finder:')
+MyTopContacts.find_contact('Пятый', 'Петька')
